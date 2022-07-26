4fd9e9ba6b83e4b0d837f7f6225982c0e4f5fb08
Jodi Denney <iarrived@gmail.com>
2022-07-26 16:52:14 -0700

    Random button return kinda random movie and release year

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
+import {useState} from 'react';
 
 
 function App() {
-  const randomGet = () => {
-    let year: number = 
-     Math.floor(Math.random() * (2022 - 1977 + 1)) + 1977; 
-    console.log(year)
+ 
+const getMovie = () => {
+  let year: number = Math.floor(Math.random() * (2022 - 1960 + 1)) + 1960; 
+
+  axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=431d8e14474b80402b2e05a0e1ae22f4&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&with_watch_monetization_types=flatrate`)
+  .then((Response) => {
+    console.log(Response.data.results[4].title, year);
+  })
+
+}
+
+  // const [data, setData] = useState({data: []});
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [err, setErr] = useState('');
+
+  // const handleClick = async () => {
+  //   setIsLoading(true);
+
+  //   try {
+  //     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=431d8e14474b80402b2e05a0e1ae22f4&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&with_watch_monetization_types=flatrate`, {
+  //       method: 'GET',
+  //       headers: {
+  //         Accept: 'application/json',
+  //       },
+  //     });
+
+  //     if (!response.ok) {
+  //       console.log(`Error! status: ${response.status}`);
+  //     }
+
+  //     const result = await response.json();
+
+  //     console.log('number of movies is:' + result.results.length +' Movies are: ', JSON.stringify(result, null, 4));
+
+  //     setData(result);
+  //   } catch (err) {
+  //     console.log(err);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
 
 
-  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -20,14 +58,14 @@ function App() {
         </p>
         <a
           className="Trello-link"
-          href="https://trello.com/b/OZob7lcI/matinee-all-day"
+          href="https://trello.com/b/OZob7lcI/matinyarn ee-all-day"
           target="_blank"
           rel="noopener noreferrer"
         >
           MAD Trello Site
         </a>
       </header>
-      <button onClick={randomGet}>Random</button>
+      <button onClick={getMovie}>Random</button>
     </div>
     
   );
