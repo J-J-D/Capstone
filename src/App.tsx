c639421ba68a0ecd4b4112bb30b80c447a0e1cf7
Jodi Denney <iarrived@gmail.com>
2022-08-04 11:52:21 -0700

    Broke user login code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
+import UserLogin from "./components/UserLogin";
+import { useState } from 'react';
+import axios from 'axios';
+
 
 
 function App() {
+  // const [login, setLogin] = useState(null);
+
+  // const loginUser =  (username) => {
+  //   axios
+  //     .post('https://matinee-all-day.herokuapp.com/users/login', username)
+  //     .then((response) => {
+  //       setLogin(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     })
+  //     }
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          You should watch: 
-        </p>
-          </header>
-        <section className ="Body">
-          <MovieDisplay/>
-        </section>
-    </div>
-    
+
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <UserLogin handleSubmission={loginUser}/> */}
+          <p>
+            You should watch: 
+          </p>
+            </header>
+          <section className ="Body">
+            <MovieDisplay/>
+          </section>
+      </div>
+      
   );
 }
 
---
+++ src/components/UserLogin.tsx
@@ -0,0 +1,36 @@
+// import axios from "axios";
+// import React, { useState } from "react";
+// import "./UserLogin.css";
+// import PropTypes from "prop-types";
+// import userEvent from "@testing-library/user-event";
+
+
+
+const userLogin = () => {
+//     const [loginData, setLoginData] = useState("");
+
+//     const loginUser = (event: Event) => {
+//         event.preventDefault();
+//         handleSubmission(loginData);
+//         setLoginData("")
+//     }
+
+
+
+
+//     return (
+//         <form onSubmit={loginUser}>
+//             <p>Please enter your username:</p>
+//             <input
+//             type="text"
+//             name = "Username"
+//             value = {loginData}
+//             onChange = setLoginData()
+//             />
+        
+
+//         </form>
+//     )
+    };
+
+export default userLogin;
