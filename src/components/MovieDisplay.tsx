21df4a058ef5b47aa14554e2e8ac71eb40a6e1b4
Amel <amelntamark@gmail.com>
2022-08-04 00:04:30 -0700

    hide movie description by default; create button to toggle display

--- src/components/MovieDisplay.tsx
+++ src/components/MovieDisplay.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import { Movie } from "../types/interfaces";
 import { useState, useEffect} from "react";
 
-const MovieDisplay = () => {
-    // const [movieTitle, setMovieTitle] = useState('');
-    const [movieDetails, setMovieDetails] = useState<Movie>();
+// TODO: return poster; create button for user to add movie to seen list
+// Should this be broken into smaller components? 
 
+const MovieDisplay = () => {
+    const [movieDetails, setMovieDetails] = useState<Movie>();
+    const [overviewVisibility, setOverviewVisibility] = useState(false);
+
+    // Make call to MAD backend for a random movie recommendation
     const getMovieData = () => {
         axios
         .get<Movie>('https://matinee-all-day.herokuapp.com/sessions/2')
@@ -19,25 +23,73 @@ const MovieDisplay = () => {
         }));
     };
 
-    return (
-        <div className="movie-display">
-            <h1 className="movie-title">
-                {/* {movieDetails} */}
-                {movieDetails?.title}
-            </h1>
-            <p>
-                {movieDetails?.release_date.slice(0, 4)}
-            </p>
-            <p>
-                {movieDetails?.overview}
-            </p>
-            <button
-            onClick={getMovieData}
-            >
-                show me a movie!
-            </button>
-        </div>
-    );
+    // Hide or reveal overview of the plot
+    const toggleOverviewVisibility = () => {
+        setOverviewVisibility(!overviewVisibility)
+    };
+
+
+    if (movieDetails === undefined) { 
+        // No movie rec has been received yet
+        return (
+            <div className="movie-display">
+                <button
+                onClick={getMovieData}
+                >
+                    show me a movie!
+                </button>
+            </div>
+        );
+    } else if (movieDetails !== undefined && overviewVisibility === false) {
+        // Movie rec has been received and the plot description is hidden
+        return (
+            <div className="movie-display">
+                <h1 className="movie-title">
+                    {movieDetails?.title}
+                </h1>
+                <p>
+                    {movieDetails?.release_date.slice(0, 4)}
+                </p>
+                <button
+                onClick={toggleOverviewVisibility}
+                >
+                    reveal plot description
+                </button>
+                <br></br>
+                <button
+                onClick={getMovieData}
+                >
+                    show me a different movie!
+                </button>
+            </div>
+        );
+    } else {
+        // Movie rec has been received and the plot description is visible
+        return (
+            <div className="movie-display">
+                <h1 className="movie-title">
+                    {movieDetails?.title}
+                </h1>
+                <p>
+                    {movieDetails?.release_date.slice(0, 4)}
+                </p>
+                <p>
+                    {movieDetails.overview}
+                </p>
+                <button
+                onClick={toggleOverviewVisibility}
+                >
+                    hide plot description
+                </button>
+                <br></br>
+                <button
+                onClick={getMovieData}
+                >
+                    show me a different movie!
+                </button>
+            </div>
+        );
+    }
 };
 
 export default MovieDisplay;
