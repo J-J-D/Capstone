1617be6f1eec42669ebd79aa89710ace8f8c600c
Amel <amelntamark@gmail.com>
2022-08-03 19:45:41 -0700

    return movie title, release year, and overview

--- src/components/MovieDisplay.tsx
+++ src/components/MovieDisplay.tsx
@@ -4,27 +4,33 @@ import { Movie } from "../types/interfaces";
 import { useState, useEffect} from "react";
 
 const MovieDisplay = () => {
-    const [movieTitle, setMovieTitle] = useState('');
+    // const [movieTitle, setMovieTitle] = useState('');
+    const [movieDetails, setMovieDetails] = useState<Movie>();
 
     const getMovieData = () => {
         axios
         .get<Movie>('https://matinee-all-day.herokuapp.com/sessions/2')
         .then((response: AxiosResponse) => {
-            console.log('Response: ', response.data)
-            setMovieTitle(response.data.title)
-        });
+            console.log(response.data)
+            setMovieDetails(response.data)
+        })
+        .catch((error => {
+            console.log(`an error occured. details: ${error}`)
+        }));
     };
-
-    // const getMovieData = async () => {
-    //     const movieResult: Movie = JSON.parse(await axios.get('https://matinee-all-day.herokuapp.com/sessions/2'));
-    //     console.log(movieResult)
-    // }
 
     return (
         <div className="movie-display">
             <h1 className="movie-title">
-                {movieTitle}
+                {/* {movieDetails} */}
+                {movieDetails?.title}
             </h1>
+            <p>
+                {movieDetails?.release_date.slice(0, 4)}
+            </p>
+            <p>
+                {movieDetails?.overview}
+            </p>
             <button
             onClick={getMovieData}
             >
