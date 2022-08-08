5e481a1c4af961e3beb19c6d70a17cd867d567d2
Jodi Denney <iarrived@gmail.com>
2022-08-08 14:03:45 -0700

    Runtime Buttons

--- src/components/Runtime.tsx
+++ src/components/Runtime.tsx
@@ -6,12 +6,22 @@ interface RuntimeProps {userPrefs: UserPrefs, setUserPrefs: React.Dispatch<React
 
 const Runtime = (props: RuntimeProps) => {
     const {userPrefs, setUserPrefs} = props
+    const handleClick = (e: React.SyntheticEvent) => {
+        console.log(e)
+        //@ts-ignore comment
+        setUserPrefs({...userPrefs, runtime: e.target.value})
+        }
+    const TMDB_MAX_TIMES = ["90 minutes", "2 hours"]
 
     return (
         <div>
             <h3>Approximate maximum movie length?</h3>
             <ul>
-                <button>90 minutes</button>
+            {TMDB_MAX_TIMES.map(time => {
+                    return (
+                    <button className = "runtimeButton" key={time} onClick = {handleClick} value = {time}>{time}</button>
+                    )
+                })}
             </ul>
 
         </div>
