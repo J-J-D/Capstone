b12f5d2fef3d96bc9c7e144807d615874d562236
Jodi Denney <iarrived@gmail.com>
2022-08-08 13:53:39 -0700

    Era Buttons. . Currently can only select one decade

--- src/components/Eras.tsx
+++ src/components/Eras.tsx
@@ -6,15 +6,23 @@ interface ErasProps {userPrefs: UserPrefs, setUserPrefs: React.Dispatch<React.Se
 
 const Eras = (props: ErasProps) => {
     const {userPrefs, setUserPrefs} = props
-    const handleClick = () => setUserPrefs({...userPrefs, era: "1970s"})
+    const handleClick = (e: React.SyntheticEvent) => {
+    console.log(e)
+    //@ts-ignore comment
+    setUserPrefs({...userPrefs, era: e.target.value})
+    }
 
+const TMDB_DECADES = ["1970s", "1980s", "1990s", "2000s", "2010s", "2020 Onward"]
 
     return (
         <div>
             <ul>
             <h3>Era Selected {userPrefs.era}</h3>
-
-                <button onClick = {handleClick}>1970s</button>
+            {TMDB_DECADES.map(decade => {
+                    return (
+                        <button className = "eraButton" key={decade} onClick = {handleClick} value = {decade}>{decade}</button>
+                    )
+                })}
             </ul>
         </div>
     )
