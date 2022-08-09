22053463a38836a032b33113bb8b4f9e0090f247
Amel <amelntamark@gmail.com>
2022-08-08 20:46:21 -0700

    successfully get movie response, but not rendering display '

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -4,7 +4,7 @@ import Eras from "./Eras";
 import Runtime from "./Runtime";
 import { UserPrefs } from "../types/interfaces";
 import axios from "axios";
-import { createModuleResolutionCache } from "typescript";
+// import { createModuleResolutionCache, moveEmitHelpers } from "typescript";
 import MovieDisplay from "./MovieDisplay";
 import { Movie } from "../types/interfaces";
 
@@ -22,45 +22,58 @@ const InputForm = (props: InputFormProps) => {
 
     const InputTitles = ["Genres", "Era", "Runtime"];
 
-    const [movieRec, setMovieRec] = useState<Movie>({
-        "id": 0,
-        "overview": "",
-        "poster": "",
-        "release_date": "",
-        "title": ""
-    });
+    // const [movieRec, setMovieRec] = useState<Movie>({
+    //     "id": 0,
+    //     "overview": "",
+    //     "poster": "",
+    //     "release_date": "",
+    //     "title": ""
+    // });
 
-    const [sessionId, setSessionId] = useState(0);
+    // const [sessionId, setSessionId] = useState(0);
 
-    const pageDisplay = () => {
+    const pageDisplay = (sessionId? : number) => {
         if (page === 0){
             return <Genres userPrefs={userPrefs} setUserPrefs={setUserPrefs}/>
         }else if(page === 1){
             return <Eras userPrefs={userPrefs} setUserPrefs={setUserPrefs}/>
         }else if(page === 2){
-            return <Runtime userPrefs={userPrefs} setUserPrefs={setUserPrefs}/>
-        }else if(page === 3){
-            return <MovieDisplay sessionId={sessionId} movieRec={movieRec}/>
-        }
+            return <Runtime userPrefs={userPrefs} setUserPrefs={setUserPrefs}/>}
+        // }else if(page === 3){
+        //     return <MovieDisplay sessionId={sessionId} movieRec={movieRec}/>
+        // }
 
-    }
+    };
 
-    const getSession = () => {
+    // const returnMovie = (sessionId: number, movieRec: Movie) => {
+    //     return <MovieDisplay sessionId={sessionId} movieRec={movieRec}/>
+    // }
+
+    const getSession = (sessionId: number) => {
+        let movieRec: Movie = {
+                "id": 0,
+                "overview": "",
+                "poster": "",
+                "release_date": "",
+                "title": ""
+            }
         axios
         .get(`https://matinee-all-day.herokuapp.com/sessions/${sessionId}`)
         .then((response) => {
-            console.log(response.data);
-            setMovieRec(response.data);
+            movieRec = response.data;
+            console.log(movieRec);
+            // setMovieRec(response.data);
             setPage(3);
         })
         .catch((err) => {console.log(err)}) 
+        return <MovieDisplay sessionId={sessionId} movieRec={movieRec}/>;
     }
 
-    const putSession = () => {
+    const putSession = (sessionId: number) => {
         axios
         .put(`https://matinee-all-day.herokuapp.com/sessions/${sessionId}`)
         .then((reponse) => {console.log(reponse)
-        getSession()})
+        getSession(sessionId)})
         .catch((err) => {console.log(err)}) 
     }
 
@@ -69,8 +82,9 @@ const InputForm = (props: InputFormProps) => {
         axios
         .post('https://matinee-all-day.herokuapp.com/sessions', postPrefs)
         .then((response) => {console.log(response);
-        setSessionId(response.data.session_id);
-        putSession();
+            let sessionId = response.data.session_id;
+        // setSessionId(response.data.session_id);
+        putSession(sessionId);
     })
         .catch((err) => {console.log(err)})
 };
