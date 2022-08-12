0c571ed4635f3a4147228945e2a84ebca0919583
Amel <amelntamark@gmail.com>
2022-08-12 13:00:21 -0700

    undo all react-router changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import InputForm from "./components/InputForm";
 import { useState } from 'react';
 import axios from 'axios';
 
-
-
-
 function App() {
 // login tracks the current user and their id. If no one logged in, the user id is set to Default User (48)
   const [login, setLogin] = useState({id: 48, message: 
